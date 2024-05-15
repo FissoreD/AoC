2be8e8f09ff8c857c832c2fd78b61cbacba6bd02
Davide Fissore <fisso9900@gmail.com>
2024-05-15 22:22:09 +0200

    y2015/d03

---
+++ lib/y2015/day03.ml
@@ -0,0 +1,37 @@
+type pos = {x:int; y:int}
+
+let move p = function
+  | '^' -> {p with y = p.y - 1}
+  | 'v' -> {p with y = p.y + 1}
+  | '>' -> {p with x = p.x + 1}
+  | '<' -> {p with x = p.x - 1}
+  | _ -> invalid_arg "invalid move"
+
+let add_pos p l = 
+  if not (List.mem !p !l) then
+      l := !p :: !l
+
+let p1 s = 
+  let s = List.hd s in
+  let c_pos = ref {x=0;y=0} in
+  let explored = ref [!c_pos] in
+  let len = String.length s in
+  for i = 0 to len - 1 do
+    c_pos := move !c_pos s.[i];
+    add_pos c_pos explored
+  done;
+  print_int @@ List.length !explored
+
+let p2 s = 
+  let s = List.hd s in
+  let c_pos = ref {x=0;y=0} in
+  let c_pos' = ref {x=0;y=0} in
+  let explored = ref [!c_pos] in
+  let len = String.length s in
+  for i = 0 to len - 1 do
+    if i mod 2 == 0 then (c_pos := move !c_pos s.[i]; add_pos c_pos explored)
+    else (c_pos' := move !c_pos' s.[i]; add_pos c_pos' explored)
+  done;
+  print_int @@ List.length !explored
+
+
--- test/y2015_test/y2015_test.ml
+++ test/y2015_test/y2015_test.ml
@@ -1,11 +1,13 @@
+open Y2015
+
+let p1_list = [Day01.p1; Day02.p1; Day03.p1]
+let p2_list = [Day01.p2; Day02.p2; Day03.p2] 
+
 let int_to_string n =
   Printf.sprintf "%02d" n
 
 let test_day day_nb =
-  let open Y2015 in
   let f_name = "day" ^ int_to_string day_nb ^ ".txt" in
-  let p1_list = [Day01.p1; Day02.p1] in
-  let p2_list = [Day01.p2; Day02.p2] in
   let p1 = List.nth p1_list (day_nb - 1) in
   let p2 = List.nth p2_list (day_nb - 1) in
   Printf.printf "Solution of day %d part 1 is " day_nb;   
@@ -17,4 +19,5 @@ let test_day day_nb =
 
 let _ =
   test_day 1;
-  test_day 2
+  test_day 2;
+  test_day 3
