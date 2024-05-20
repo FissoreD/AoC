5ab4fb295fd2f2a4959d45979ee2033cf8028ed1
Davide Fissore <fisso9900@gmail.com>
2024-05-20 22:45:55 +0200

    y2015/d07

---
+++ lib/y2015/day07.ml
@@ -0,0 +1,63 @@
+type int_str = Int of int | Str of string
+
+type instr = 
+  | Affect of int_str 
+  | Unary of (int -> int) * int_str 
+  | Binary of  (int -> int -> int) * int_str * int_str
+
+module Mem = Map.Make(String)
+
+let get m = function | Int i -> i | Str s -> Mem.find s m
+
+let run_line mem s = function 
+  | Affect n -> Mem.add s (get mem n) mem
+  | Unary (f, s1) -> Mem.add s (f (get mem s1)) mem
+  | Binary (f, s1, s2) -> Mem.add s (f (get mem s1) (get mem s2)) mem
+
+let rec run_lines_aux mem = function 
+  | [] -> mem, []
+  | (instr, ad) :: tl -> 
+      try 
+        let mem = run_line mem ad instr in
+        run_lines_aux mem tl
+      with Not_found ->
+        let mem, tl = run_lines_aux mem tl in
+        mem, (instr, ad) :: tl
+
+let rec run_lines mem l = 
+  let (mem, l) = run_lines_aux mem l in 
+  if l = [] then mem else run_lines mem l
+
+let to_int_str s =
+  match int_of_string_opt s with None -> Str s | Some s -> Int s
+
+let parse_unary = function
+  | "NOT" -> fun x -> -x - 1
+  | a -> failwith ("Not implemented unary operator " ^ a)
+
+let parse_binary = function
+  | "AND" -> (land)
+  | "OR" -> (lor)
+  | "LSHIFT" -> (lsl)
+  | "RSHIFT" -> (lsr)
+  | a -> failwith ("Not implemented binary operator " ^ a)
+
+let parse_line l = 
+  match String.split_on_char ' ' l with
+  | [s;"->";ad] -> Affect (to_int_str s), ad
+  | [un;s;"->";ad] -> Unary ((parse_unary un), to_int_str s), ad
+  | [s1; bin; s2; "->"; ad] -> Binary ((parse_binary bin), to_int_str s1, to_int_str s2), ad
+  | l -> List.fold_left (^) "" l |> failwith
+
+let run_lines l = 
+  let mem = Mem.empty in
+  run_lines mem l |> Mem.find "a"
+
+let p1 l = run_lines (List.map parse_line l) |> print_int
+
+let p2 l =
+  let l = List.map parse_line l in
+  let val_a = run_lines l in
+  let l = List.map (function (_, "b") -> (Affect (Int val_a), "b") | a -> a) l in
+  run_lines l |> print_int
+
--- test/y2015_test/y2015_test.ml
+++ test/y2015_test/y2015_test.ml
@@ -1,7 +1,7 @@
 open Y2015
 
-let p1_list = [Day01.p1; Day02.p1; Day03.p1; Day04.p1; Day05.p1; Day06.p1]
-let p2_list = [Day01.p2; Day02.p2; Day03.p2; Day04.p2; Day05.p2; Day06.p2] 
+let p1_list = [Day01.p1; Day02.p1; Day03.p1; Day04.p1; Day05.p1; Day06.p1; Day07.p1]
+let p2_list = [Day01.p2; Day02.p2; Day03.p2; Day04.p2; Day05.p2; Day06.p2; Day07.p2] 
 
 let int_to_string n =
   Printf.sprintf "%02d" n
@@ -18,7 +18,7 @@ let test_day day_nb =
   print_newline ()
 
 let _ =
-  let day_nb = 6 in
+  let day_nb = 7 in
   for i = 1 to day_nb do
     test_day i
   done
