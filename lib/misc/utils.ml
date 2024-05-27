7f2f4e895ca6ec5d0eb6692e992bf09af8c1528a
Davide Fissore <fisso9900@gmail.com>
2024-05-27 22:49:41 +0200

    y2015/d15

---
+++ lib/misc/utils.ml
@@ -0,0 +1,93 @@
+(** return all ints in a string  *)
+let all_ints ?(only_pos=false) s =
+  let match_rex = Str.regexp
+    (if only_pos then "[0-9][0-9]*" 
+    else "-?[0-9][0-9]*") in
+  let rec aux p = 
+    try
+      let x = Str.search_forward match_rex s p in
+      let old_match = Str.matched_string s in
+      int_of_string old_match :: aux (x + String.length old_match)
+    with Not_found -> [] in
+  aux 0
+
+let pp_list ?(endL="") ?(sep=" ") ?(delimit=false) f l =
+  let open Format in
+  let hd = if delimit then "[" else "" in
+  let tl = if delimit then "]" else "" in
+  asprintf "%s%a%s%s" hd (pp_print_list ~pp_sep:(fun fmt _ -> pp_print_string fmt sep) 
+    (fun fmt x -> Format.fprintf fmt "%s" (f x))) l tl endL
+
+
+let pp_array ?(endL="") ?(sep=" ") ?(delimit=false) f l =
+  pp_list ~endL ~sep ~delimit f (Array.to_list l)
+
+(** returns all the tuples with length nb_items summing up to max_sum
+    
+    for example:
+      [all_combo ~max_sum:3 ~nb_items:2] gives [[[0 3] [1 2] [2 1] [3 0]]]
+*)
+let all_combo ~max_sum ~nb_items : int list list =
+
+  let module MS = Set.Make(String) in
+  let s = ref MS.empty in
+  let add x = s := MS.add x !s in 
+  let add_l l = List.fold_left (Printf.sprintf "%s_%d") "" l |> add in
+  let mem l = MS.mem (List.fold_left (Printf.sprintf "%s_%d") "" l) !s in
+
+  let rec add_one_all (l: int list) : int list list = match l with
+    | [] -> []
+    | hd :: tl ->
+      ((hd + 1) :: tl) :: (List.map (fun x -> hd :: x) (add_one_all tl)) in
+
+  let distr_one (l: int list) : int list list = match l with
+    | [] -> []
+    | hd :: l -> List.map (fun x -> hd - 1 :: x) (add_one_all l) in
+
+  let res = ref [] in
+
+  let rec repeat_build_list n (l: int list) = 
+    if n = -1 || mem l then ()
+    else (add_l l;
+      res := l :: !res; 
+      let ll = distr_one l in
+      List.iter (repeat_build_list (n-1)) ll) in
+
+  let l = max_sum :: List.init (nb_items-1) (fun _ -> 0) in
+  repeat_build_list max_sum l;
+
+  !res
+
+(* same all_combo but returning a list of arrays *)
+let all_combo_arr ~max_sum ~nb_items : int array list =
+
+  let module MS = Set.Make(String) in
+  let s = ref MS.empty in
+  let add x = s := MS.add x !s in 
+  let add_l l = Array.fold_left (Printf.sprintf "%s_%d") "" l |> add in
+  let mem l = MS.mem (Array.fold_left (Printf.sprintf "%s_%d") "" l) !s in
+
+  let add_one_all hd (l: int array) : int array array = 
+    let res = Array.init (Array.length l-1) 
+      (fun x -> Array.init (Array.length l) 
+        (fun n -> if n = 0 then hd else if n = (x+1) then l.(n) + 1 else l.(n))) in 
+    res
+  in
+
+  let distr_one (l: int array) : int array array = 
+    if Array.length l = 0 then Array.make 0 (Array.make 0 0)
+    else add_one_all (l.(0) - 1) l in
+
+  let res = ref [] in
+
+  let rec repeat_build_list n (l: int array) = 
+    if n = -1 || mem l then ()
+    else (add_l l;
+      res := l :: !res; 
+      let ll = distr_one l in
+      Array.iter (repeat_build_list (n-1)) ll) in
+
+  let l = Array.init nb_items (function 0 -> max_sum | _ -> 0) in
+  repeat_build_list max_sum l;
+
+  !res
---
+++ lib/y2015/day15.ml
@@ -0,0 +1,19 @@
+let parse_line l = Misc.Utils.all_ints l |> List.rev (* calories are put first *)
+
+let get_value =
+  List.(fold_left2 (fun a x y -> map2 (fun a x -> a + y * x) a x) [0;0;0;0;0])
+
+let get_best l =   
+  let mul a = List.map (max 0) (List.tl a) |> List.fold_left ( * ) 1 in
+  List.map mul l |> List.fold_left max min_int
+
+let get_quantities l = 
+  let infos = List.map parse_line l in
+  let combos = Misc.Utils.all_combo ~max_sum:100 ~nb_items:(List.length infos) in
+  List.map (get_value infos) combos
+
+let p1 l = get_quantities l |> get_best |> print_int 
+
+let p2 l =
+  get_quantities l |> List.filter (fun e -> List.hd e = 500)
+    |> get_best |> print_int 
