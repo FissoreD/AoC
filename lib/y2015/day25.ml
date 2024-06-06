97c00d1c42f8c206a65c12def5f68aac7a072ae2
Davide Fissore <fisso9900@gmail.com>
2024-06-06 19:15:55 +0200

    y2016/d01

---
+++ lib/y2015/day25.ml
@@ -0,0 +1,23 @@
+let init, coeff, div = (20151125, 252533, 33554393)
+
+let pos l =
+  match Misc.Utils.all_ints (List.hd l) with
+  | [ x; y ] -> (x, y)
+  | _ -> failwith "y15/d25: invalid entry"
+
+
+let pos2int x y = 
+  let z = x + y - 1 in
+  z * (z+1) / 2 - x - 1
+
+let next n = n * coeff mod div
+
+let p1 l =
+  let y, x = pos l in
+  let r = ref init in
+  for _ = 1 to pos2int x y do
+    r := next !r
+  done;
+  print_int !r
+
+let p2 _ = print_string "END 2015!"
