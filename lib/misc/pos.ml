f62246e77a4590f7ded2e89e59704afacb6d3b21
Davide Fissore <fisso9900@gmail.com>
2024-06-21 22:59:08 +0200

    y2016/d13

---
+++ lib/misc/pos.ml
@@ -0,0 +1,29 @@
+let neigh4 = [ (1, 0); (-1, 0); (0, 1); (0, -1) ]
+let neigh8 = neigh4 @ [ (1, 1); (-1, 1); (1, -1); (-1, -1) ]
+let valid00 (x, y) = x >= 0 && y >= 0
+
+let valid_mat m ((x, y) as p) =
+  let w, h = (List.length (List.hd m), List.length m) in
+  valid00 p && x < w && y < h
+
+type t = int * int
+
+module type M = sig
+  val neigh : t list
+  val valid : t -> bool
+end
+
+module Make (M : M) = struct
+  let add (x, y) (x', y') = (x + x', y + y')
+  let get_neigh p = List.map (add p) M.neigh |> List.filter M.valid
+  let valid = M.valid
+end
+
+module CmpPos : Set.OrderedType = struct
+  type t = int * int
+
+  let compare (x, y) (x', y') =
+    match compare x x' with 0 -> compare y y' | n -> n
+end
+
+module SetPos = Set.Make (CmpPos)
--- lib/y2016/day13.ml
+++ lib/y2016/day13.ml
@@ -1,3 +1,39 @@
-let p1 _ = ""
+let parse l = List.hd l |> int_of_string
+let rec ones = function 0 -> 0 | n -> (n land 1) + ones (n lsr 1)
 
-let p2 _ = ""
+module M (S : sig
+  val fn : int
+end) =
+struct
+  let explored = ref []
+  let add_explored p = explored := p :: !explored
+
+  let is_free (x, y) =
+    let f x y = (x * x) + (3 * x) + (2 * x * y) + y + (y * y) in
+    f x y + S.fn |> ones |> fun x -> x mod 2 = 0
+
+  let neigh = Pos.neigh4
+  let valid p = Pos.valid00 p && is_free p && not (List.mem p !explored)
+end
+
+let bfs is_p1 fn =
+  let module X = M (struct
+    let fn = fn
+  end) in
+  let module Pos = Pos.Make (X) in
+  let dist = ref 0 in
+  let pos = ref [ (1, 1) ] in
+  let while_cond =
+    if is_p1 then fun () -> not (List.mem (31, 39) !pos)
+    else fun () -> !dist <= 50
+  in
+  while while_cond () do
+    List.iter X.add_explored !pos;
+    pos := Utils.no_dup (List.map Pos.get_neigh !pos |> List.flatten);
+    incr dist;
+    if !pos = [] then failwith "y2016/d13 bfs error"
+  done;
+  if is_p1 then !dist else List.length !X.explored
+
+let p1 n = bfs true (parse n) |> string_of_int
+let p2 n = bfs false (parse n) |> string_of_int
