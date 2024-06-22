ccb9b165341a52df294889f0b42e0d25ded5ffa0
Davide Fissore <fisso9900@gmail.com>
2024-06-22 23:23:42 +0200

    y2016/d15

--- lib/y2016/day15.ml
+++ lib/y2016/day15.ml
@@ -1,3 +1,28 @@
-let p1 _ = ""
+let enter_disc (nb, size, pos) = (size - ((nb + pos) mod size), size)
 
-let p2 _ = ""
+let parse l =
+  match Utils.all_ints l with
+  | [ n; size; _; pos ] -> (n, size, pos) (* Ignore time since always 0 *)
+  | _ -> failwith "y2016/d15: Invalid arg"
+
+let rec gcd a b = if b = 0 then a else gcd b (a mod b)
+let lcm a b = a * b / gcd a b
+
+let rec align (m, sm) (n, sn) =
+  if m = n then (n, lcm sm sn)
+  else if m > n then align (m, sm) (n + sn, sn)
+  else align (m + sm, sm) (n, sn)
+
+let align_all l =
+  let l = List.map enter_disc l in
+  let rec align_l p = function
+    | [] -> fst p
+    | hd :: tl -> align_l (align p hd) tl
+  in
+  align_l (List.hd l) (List.tl l)
+
+let p1 l = List.map parse l |> align_all |> string_of_int
+
+let p2 l =
+  let l = List.map parse l @ [ (7, 11, 0) ] in
+  align_all l |> string_of_int
