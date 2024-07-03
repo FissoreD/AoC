e0c6aea87dd2fcd76d00d2a12e2cc042bf073a90
Davide Fissore <fisso9900@gmail.com>
2024-07-03 21:34:44 +0200

    y2017/d01

--- lib/y2017/day01.ml
+++ lib/y2017/day01.ml
@@ -1,3 +1,12 @@
-let p1 _ = ""
+let parse l = String.to_array (List.hd l)
 
-let p2 _ = ""
+let count ar step =
+  let res, len = (ref 0, Array.length ar) in
+  for i = 0 to len - 1 do
+    if ar.(i) = ar.((i + step) mod len) then
+      res := !res + int_of_char ar.(i) - int_of_char '0'
+  done;
+  !res
+
+let p1 l = count (parse l) 1 |> string_of_int
+let p2 l = count (parse l) (String.length (List.hd l) / 2) |> string_of_int
