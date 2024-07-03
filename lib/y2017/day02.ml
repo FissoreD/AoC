c1c0820a5c8512a935a1f6a7a9bd5fc1a7a2d51a
Davide Fissore <fisso9900@gmail.com>
2024-07-03 21:47:25 +0200

    y2017/d02

--- lib/y2017/day02.ml
+++ lib/y2017/day02.ml
@@ -1,3 +1,13 @@
-let p1 _ = ""
+let parse l = List.map Utils.all_ints l
 
-let p2 _ = ""
+let p1 l =
+  let l = parse l in
+  List.(sum (map max l) - sum (map min l)) |> string_of_int
+
+let find_pair x = List.find (fun y -> x mod y = 0)
+
+let rec find_pair_l p l =
+  let x, xs = (List.hd l, List.tl l) in
+  try x / find_pair x (p @ xs) with Not_found -> find_pair_l (x :: p) xs
+
+let p2 l = List.(sum @@ map (find_pair_l []) (parse l)) |> string_of_int
