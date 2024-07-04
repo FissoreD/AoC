c7a09f65b0e629ee8f2b76a8561533027e50a19d
Davide Fissore <fisso9900@gmail.com>
2024-07-04 19:10:42 +0200

    y2017/d05

--- lib/y2017/day05.ml
+++ lib/y2017/day05.ml
@@ -1,3 +1,19 @@
-let p1 _ = ""
+let incr_ar ar pos =
+  ar.(pos) <- ar.(pos) + 1;
+  ar
 
-let p2 _ = ""
+let rec jumps ar c pos =
+  if pos < 0 || pos >= Array.length ar then c
+  else jumps (incr_ar ar pos) (c + 1) (pos + ar.(pos))
+
+let p1 l = jumps (Array.of_list (List.map int_of_string l)) 0 0 |> string_of_int
+
+let incr_ar ar pos =
+  ar.(pos) <- (ar.(pos) + if ar.(pos) >= 3 then -1 else 1);
+  ar
+
+let rec jumps ar c pos =
+  if pos < 0 || pos >= Array.length ar then c
+  else jumps (incr_ar ar pos) (c + 1) (pos + ar.(pos))
+
+let p2 l = jumps (Array.of_list (List.map int_of_string l)) 0 0 |> string_of_int
