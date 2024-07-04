e065619787cdc86be411652988e36a98c428b71d
Davide Fissore <fisso9900@gmail.com>
2024-07-04 18:50:12 +0200

    y2017/d04

--- lib/y2017/day04.ml
+++ lib/y2017/day04.ml
@@ -1,3 +1,12 @@
-let p1 _ = ""
+let rec valid = function
+  | [] -> true
+  | x :: xs -> (not (List.mem x xs)) && valid xs
 
-let p2 _ = ""
+let parse = String.split_on_char ' '
+let p1 l = List.(filter valid (map parse l) |> length) |> string_of_int
+
+let p2 l =
+  let open List in
+  let sort_str x = String.to_list x |> sort Char.compare in
+  let split_sort x = parse x |> map sort_str in
+  filter valid (map split_sort l) |> length |> string_of_int
