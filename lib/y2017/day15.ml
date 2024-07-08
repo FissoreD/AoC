ccf47afc091028243e614ba7eb41db1c24341d89
Davide Fissore <fisso9900@gmail.com>
2024-07-08 22:20:54 +0200

    y2017/d15

--- lib/y2017/day15.ml
+++ lib/y2017/day15.ml
@@ -1,3 +1,24 @@
-let p1 _ = ""
+let nextA n = n * 16807 mod 2_147_483_647
+let nextB n = n * 48271 mod 2_147_483_647
 
-let p2 _ = ""
+let same a b =
+  let mask = (2 lsl 15) - 1 in
+  if a land mask = b land mask then 1 else 0
+
+let parse l = List.map (fun i -> List.hd (Utils.all_ints i)) l |> Tuple.to2
+
+let rec repeat fa fb acc (a, b) = function
+  | 0 -> acc + same a b
+  | n -> repeat fa fb (same a b + acc) (fa a, fb b) (n - 1)
+
+let p1 l = repeat nextA nextB 0 (parse l) 40_000_000 |> string_of_int
+
+let rec nextA' n =
+  let r = n * 16807 mod 2_147_483_647 in
+  if r mod 4 = 0 then r else nextA' r
+
+let rec nextB' n =
+  let r = n * 48271 mod 2_147_483_647 in
+  if r mod 8 = 0 then r else nextB' r
+
+let p2 l = repeat nextA' nextB' 0 (parse l) 5_000_000 |> string_of_int
