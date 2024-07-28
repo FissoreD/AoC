9e9fef45371a5085e8ec9d88360f27325f90b4a8
Davide Fissore <fisso9900@gmail.com>
2024-07-29 00:04:37 +0200

    y2018/d01

--- lib/y2018/day01.ml
+++ lib/y2018/day01.ml
@@ -1,2 +1,15 @@
-let p1 _ = ""
-let p2 _ = ""
+let p1 l = List.map int_of_string l |> List.sum |> string_of_int
+
+let p2 l =
+  let all = List.map int_of_string l in
+  let mem = Hashtbl.create 1024 in
+  let rec find_twice acc l =
+    if Hashtbl.mem mem acc then acc
+    else
+      match l with
+      | [] -> find_twice acc all
+      | x :: xs ->
+          Hashtbl.replace mem acc ();
+          find_twice (x + acc) xs
+  in
+  find_twice 0 all |> string_of_int
