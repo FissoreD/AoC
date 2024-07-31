8e39d42c13502125e39c4c7d9453251a0ab0ee21
Davide Fissore <fisso9900@gmail.com>
2024-07-31 21:13:14 +0200

    y2018/d05

--- lib/y2018/day05.ml
+++ lib/y2018/day05.ml
@@ -1,2 +1,28 @@
-let p1 _ = ""
-let p2 _ = ""
+let parse l = List.hd l |> String.to_list |> List.map int_of_char
+let diff = int_of_char 'a' - int_of_char 'A' |> abs
+let react a b = abs (a - b) = diff
+
+let rec react_list = function
+  | [] -> []
+  | [ a ] -> [ a ]
+  | x1 :: x2 :: xs when react x1 x2 -> react_list xs
+  | x :: xs -> x :: react_list xs
+
+let rec full_react l =
+  let l' = react_list l in
+  if l = l' then l else full_react l'
+
+let full_react_len l = List.length (full_react l)
+let p1 l = full_react_len (parse l) |> string_of_int
+
+let remove n l =
+  List.filter (fun e -> e <> n && e <> n + diff && e <> n - diff) l
+
+let p2 l =
+  let l = parse l |> full_react in
+  let start, stop = (int_of_char 'a', int_of_char 'z' + 1) in
+  let rec aux i =
+    if i = stop then max_int
+    else min (full_react_len (remove i l)) (aux (i + 1))
+  in
+  aux start |> string_of_int
